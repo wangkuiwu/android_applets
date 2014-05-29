b60e476f55d1e33823267c41305a5fc2788f83a1
skywang <skywang@wondermedia.com.cn>
2014-05-29 14:49:57 +0800

    different language apk.

---
+++ training/01_getting_started/03_different_devices/01_languages/app/src/com/skw/language/LanguageTest.java
@@ -0,0 +1,15 @@
+package com.skw.language;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LanguageTest extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
