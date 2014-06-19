1007efecee8cc3dfe469a4546320c11c672524a6
skywang <skywang@wondermedia.com.cn>
2014-06-19 11:45:37 +0800

    Update RadioButton.

--- api_guide/ui/widgets/RadioButton/RadioTest/src/com/skw/radiotest/RadioTest.java
+++ api_guide/ui/widgets/RadioButton/RadioTest/src/com/skw/radiotest/RadioTest.java
@@ -5,13 +5,33 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.RadioButton;
 import android.widget.Toast;
+import android.widget.TextView;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 
 public class RadioTest extends Activity {
+
+    private TextView mView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        mView = (TextView) findViewById(R.id.tv_intro);
+
+        RadioButton mTwo = (RadioButton) findViewById(R.id.radio_two);
+        mTwo.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    mView.setText("two checked!");
+                } else {
+                    mView.setText("two un-checked!");
+                }
+            }
+        });
+ 
     }
 
 
