d9eec54c619b44048a5095d1797abdb50c4b90cc
skywang <wangkuiwu@gmail.com>
2014-06-21 16:41:09 +0800

    ActionBar: basic use.

---
+++ api_guide/ui/actionbar/01_basic/BarTest/src/com/skw/bartest/BarTest.java
@@ -0,0 +1,43 @@
+package com.skw.bartest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+import android.view.Menu;
+import android.view.MenuItem;  
+import android.view.MenuInflater; 
+
+public class BarTest extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.search:
+                Toast.makeText(getApplicationContext(), "search", 0).show();
+                return true;
+            case R.id.setting:
+                Toast.makeText(getApplicationContext(), "setting", 0).show();
+                return true;
+            case R.id.share:
+                Toast.makeText(getApplicationContext(), "share", 0).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+}
