523c353bde6ff3abf756f4b49831c250219259bc
skywang <skywang@wondermedia.com.cn>
2015-01-28 18:13:41 +0800

    TouchEvent: 01, basic demo.

---
+++ api_guide/app_components/events/01_event_default/EventTest/src/com/skw/eventtest/EventTest.java
@@ -0,0 +1,59 @@
+package com.skw.eventtest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+
+/**
+ * 默认情况下Activity, ViewGroup, View对事件的处理情况
+ *
+ *
+ * 一、DOWN事件的处理流程(所有函数皆返回false)
+ *         Activity                    ViewGroup                     View
+ *         1.dispatchTouchEvent
+ *                                     2. dispatchTouchEvent
+ *                                     3. onInterceptTouchEvent
+ *                                                                   4. dispatchTouchEvent
+ *                                                                   5. onTouchEvent
+ *                                     6. onTouchEvent
+ *         7.onTouchEvent
+ *
+ * 二、MOVE事件的处理流程(所有函数皆返回false)
+ *         Activity                    ViewGroup                     View
+ *         1.dispatchTouchEvent
+ *         2.onTouchEvent
+ *
+ * 三、UP事件的处理流程(所有函数皆返回false)
+ *         Activity                    ViewGroup                     View
+ *         1.dispatchTouchEvent
+ *         2.onTouchEvent
+ *
+ */
+public class EventTest extends Activity {
+    private static final String TAG = "##skywang-Activity";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+ 
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        String actionName = Utils.getActionName(event);
+        Log.d(TAG, "dispatchTouchEvent(start) :"+actionName);
+        boolean ret = super.dispatchTouchEvent(event);
+        Log.d(TAG, "dispatchTouchEvent( end ) :"+actionName+", ret="+ret);
+        return ret;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        String actionName = Utils.getActionName(event);
+        Log.d(TAG, "onTouchEvent(start) :"+actionName);
+        boolean ret = super.onTouchEvent(event);
+        Log.d(TAG, "onTouchEvent( end ) :"+actionName+", ret="+ret);
+        return ret;
+    }
+}
---
+++ api_guide/app_components/events/01_event_default/EventTest/src/com/skw/eventtest/MyView.java
@@ -0,0 +1,37 @@
+package com.skw.eventtest;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class MyView extends View {
+    private static final String TAG = "##skywang-MyView";
+
+    public MyView(Context context) {
+        super(context);
+    }
+    
+    public MyView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+ 
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        String actionName = Utils.getActionName(event);
+        Log.d(TAG, "dispatchTouchEvent(start) :"+actionName);
+        boolean ret = super.dispatchTouchEvent(event);
+        Log.d(TAG, "dispatchTouchEvent( end ) :"+actionName+", ret="+ret);
+        return ret;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        String actionName = Utils.getActionName(event);
+        Log.d(TAG, "onTouchEvent(start) :"+actionName);
+        boolean ret = super.onTouchEvent(event);
+        Log.d(TAG, "onTouchEvent( end ) :"+actionName+", ret="+ret);
+        return ret;
+    }
+}
